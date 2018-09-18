e42db0f6841d8028e7e05419a56dac3a60fd9790
cjhong <cjhong6@gmail.com>
2018-09-17 22:58:42 -0700

    Parse Setup and Authentication

---
+++ instagram/LoginViewController.swift
@@ -0,0 +1,64 @@
+//
+//  LoginViewController.swift
+//  instagram
+//
+//  Created by Chengjiu Hong on 9/17/18.
+//  Copyright © 2018 Chengjiu Hong. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+
+        // initialize a user object
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = userNameField.text
+        newUser.password = passwordField.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+        
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = userNameField.text ?? ""
+        let password = passwordField.text ?? ""
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+            } else {
+                print("User logged in successfully")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+    
+
+}
